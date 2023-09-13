dc8e398185f3c082617cca66a1ad8bd317f4de2f
olliebalabil <oliverbalabil@gmail.com>
2023-09-13 14:23:01 +0100

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //Fruit API
 require("dotenv").config
-
+const cors = require("cors")
 const fruits = require('./fruits.json')
 const express = require("express");
 const app = express();
 const port = process.env.PORT
 
+app.use(cors());
 app.use(express.json())
 
 app.get('/', (req, res) => {
